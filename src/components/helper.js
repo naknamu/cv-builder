74b7ccba69456f35debb7613befec041a3bb1b8c
naknamu <jodeldelvalle@gmail.com>
2022-11-24 17:49:39 +0800

    Edited font size for the output cv

--- src/components/helper.js
+++ src/components/helper.js
@@ -3,8 +3,8 @@ const Helper = (props) => {
     return ( 
         <div className="helper">
             <button onClick={props.onClick}>Print/Save PDF</button>
-            <button onClick={props.handleExampleOutput} type="submit">Example</button>
-            <button onClick={props.handleResetOutput} type="reset">Reset</button>
+            <button className="red" onClick={props.handleExampleOutput} type="submit">Example</button>
+            <button className="red" onClick={props.handleResetOutput} type="reset">Reset</button>
         </div>
      );
 }
